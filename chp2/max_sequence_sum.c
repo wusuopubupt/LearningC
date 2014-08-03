6e766e0953f7f88fc0681ca0a8f74d395d6d453c
wusuopubupt <wdxbupt2009@gmail.com>
2014-08-03 17:19:55 +0800

    add max_sequence_sum.c

---
+++ chp2/max_sequence_sum.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int max_subsequence_sum(const int*a, int n) {
+    int this_sum, max_sum, j;
+    this_sum = max_sum = 0;
+    for(j = 0; j < n; j++) {
+        this_sum += a[j];
+        if(this_sum > max_sum) {
+            max_sum = this_sum;
+        }
+        else if(this_sum < 0){
+            this_sum = 0;
+        }
+    }
+    return max_sum;
+}
+
+int main() {
+    int a[5] = {2, 4, -2, 6, -3};
+    int max = max_subsequence_sum(a, 5);
+    printf("max: %d\n", max);
+
+    return 0;
+}
