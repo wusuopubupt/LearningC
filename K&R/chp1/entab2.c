5b2c4970207edcf6580e8f19d0b48b9fca6d4f2b
wusuopubupt <wdxbupt2009@gmail.com>
2014-08-15 01:04:11 +0800

    my entab

---
+++ K&R/chp1/entab2.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+#define TABNUM 8
+
+int main() {
+	int c, nb, nt;
+	nb = 0;
+	nt = 0;
+
+	//a       9   3           11
+	while((c = getchar()) != EOF) {
+		if(c == ' ') {
+			nb++;
+		}
+		else {
+			nt = nb / TABNUM;
+			nb = nb % TABNUM;
+
+			//printf("nb:%d, nt:%d\n", nb, nt);
+
+			while(nt) {
+				putchar('\t');
+				nt--;
+			}
+			while(nb) {
+				putchar(' ');
+				nb--;
+			}
+			putchar(c);
+		}
+	}
+
+	return 0;
+}
