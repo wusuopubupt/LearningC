5c94e225c55129eb57cae5c5bc74114fc5ca4e13
wusuopubupt <wdxbupt2009@gmail.com>
2014-08-22 11:29:37 +0800

    bit count with while

--- K&R/chp2/bit_count.c
+++ K&R/chp2/bit_count.c
@@ -2,8 +2,18 @@
 
 int bit_count(int x) {
     int count = 0;
-	for(count = 0; x != 0; x&=(x-1)) {
+	for(count = 0; x != 0; x &= (x-1)) {
 		count++;
+	}
+	return count;
+}
+
+int bit_count2(int x) {
+	int count = 0;
+	while(x) {
+		if((x &= (x-1)) != 0) {
+			count++;
+		}
 	}
 	return count;
 }
