34ea031a88c4003693fd4ca84c506762cf9ddcef
wusuopubupt <wdxbupt2009@gmail.com>
2014-08-23 12:46:44 +0800

    update any.c

--- K&R/chp2/any.c
+++ K&R/chp2/any.c
@@ -1,14 +1,33 @@
-#include <stdio.h>
+/*
+ *  Write the function any(s1,s2), which returns the first location in
+ * the string s1 where any character from the string s2 occurs, or -1 if
+ * s1 contains no characters from s2. (The standard library function
+ * strpbrk does the same job but returns a pointer to the location.)
+ */
 
-int any(char s1[], char s2[]) {
-	int i, j;
-	for(i = 0; s1[i] != '\0'; i++) {
-		for(j = 0; s2[j] != '\0'; j++) {
-			if(s1[i] == s2[j]) {
-				return i;
-			}
-		}
-	}
-	
-	return -1;
+#include <stdio.h>
+#include <stdlib.h>
+
+int any(char s1[], char s2[]);
+
+int main(void)
+{
+    char s1[] = "abcdef";
+    char s2[] = "def";
+    printf("%d\n", any(s1, s2));
+
+    return EXIT_SUCCESS;
+}
+
+/* any:  returns the first location in s1 where any character from s2
+         occurs, or -1 if s1 contains no characters from s2. */
+int any(char s1[], char s2[])
+{
+    int i, k;
+
+    for (i = 0; s1[i] != '\0'; i++)
+        for (k = 0; s2[k] != '\0'; k++)
+            if (s1[i] == s2[k])
+                return i;
+    return -1; /* s1 contains no characters from s2 */
 }
