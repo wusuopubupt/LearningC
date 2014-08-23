f619b00759cf4b3b989a1cd207642b3e9e5448ba
wusuopubupt <wdxbupt2009@gmail.com>
2014-08-23 12:33:32 +0800

    update squeeze.c

--- K&R/chp2/squeeze.c
+++ K&R/chp2/squeeze.c
@@ -1,14 +1,45 @@
+/*
+ * Write an alternate version of squeeze(s1,s2) that deletes each
+ * character in the string s1 that matches any character in the string s2.
+ *
+ */
+
 #include <stdio.h>
 
 void squeeze(char s1[], char s2[]) {
-    int i, j, k;
-    for(i = k = 0; s1[i] != '\0'; i++) {
-        for(j = 0; s2[j] != '\0' && s2[j] != s1[i]; j++) {
-            ;
-        }
-        if(s2[j] == '\0') {
-            s1[k++] = s1[i];
-        }
-    }
-    s1[k] = '\0';
+	int i, j, k;
+	i = 0;
+	j = 0;
+	k = 0;
+	char c1, c2;
+	while((c1 = s1[i]) != '\0') {
+		j = 0;
+		while((c2 = s2[j]) != '\0') {
+			if(c1 == c2) {
+				break;
+			}
+			j++;
+		}
+		if(c2 == '\0') {
+			s1[k++] = c1;
+		}
+
+		i++;
+	}
+
+	s1[k] = '\0';
+}
+
+int main() {
+//	char s1[5] = {'a', 'b', 'c', 'd'};
+//	char s2[5] = {'x', 'a', 'p', 'c'};
+
+	char s1[] = "abcd";
+	char s2[] = "xapc";
+
+	squeeze(s1, s2);
+
+	printf("%s\n", s1);
+
+	return 0;
 }
