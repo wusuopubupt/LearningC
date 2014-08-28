26378dc3012a8452267b0e7ee1c173b7f689b545
wusuopubupt <wdxbupt2009@gmail.com>
2014-08-28 14:19:47 +0800

    create strend.c

---
+++ K&R/chp5/strend.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int strend(char *s, char *t) {
+	int len_s = 0;
+	int len_t = 0;
+
+	while(*s != '\0') {
+		s++;
+		len_s++;
+	}
+	while(*t != '\0') {
+		t++;
+		len_t++;
+	}
+	if(len_s < len_t) {
+		return 0;
+	}
+	else {
+		int i = len_t;
+		while(i) {
+			if(*s == *t) {
+				s--;
+				t--;
+			}
+			else {
+				return 0;
+			}
+			i--;
+		}
+		if(i == 0) {
+			return 1;
+		}
+	}
+
+
+}
+
+int main() {
+	char *s = "abcdef";
+	char *t = "def";
+
+	printf("%d\n", strend(s, t));
+	return 0;
+}
