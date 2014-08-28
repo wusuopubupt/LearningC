aad95a3d318f3c6f118377075692ea6e78c9e2ff
wusuopubupt <wdxbupt2009@gmail.com>
2014-08-28 14:20:45 +0800

    update strend.c

--- K&R/chp5/strend.c
+++ K&R/chp5/strend.c
@@ -1,3 +1,7 @@
+/*
+ * Write the function strend(s,t), which returns 1 if the string t
+ * occurs at the end of the string s, and zero otherwise.
+ */
 #include <stdio.h>
 
 int strend(char *s, char *t) {
