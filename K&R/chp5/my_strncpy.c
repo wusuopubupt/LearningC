a3ff2ec75c4115181d7713f839e62ceed1f935e9
wusuopubupt <wdxbupt2009@gmail.com>
2014-08-28 14:25:27 +0800

    create my_strncpy.c

---
+++ K&R/chp5/my_strncpy.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+void my_strncpy(char *s, const char *t, int n) {
+	int i = 0;
+	while(*s) {
+		s++;
+	}
+	while(*t && i < n) {
+		*s = *t;
+		s++;
+		t++;
+		i++;
+	}
+	*s = '\0';
+}
+
+int main() {
+	char s[] = "abc";
+	char t[] = "def";
+
+	my_strncpy(s, t, 2);
+	printf("s: %s, t: %s\n", s, t);
+
+	return 0;
+}
