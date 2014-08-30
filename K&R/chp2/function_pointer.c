78cf239685cda8722d9f8c143357681e2c52d3dc
wusuopubupt <wdxbupt2009@gmail.com>
2014-08-30 16:18:14 +0800

    update function_pointer.c

--- K&R/chp2/function_pointer.c
+++ K&R/chp2/function_pointer.c
@@ -6,6 +6,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef int(*fp_operation)(int a, int b);
+
+int add(int a, int b);
+int subtract(int a, int b);
+int compute(fp_operation op, int a, int b);
+
+int main() {
+	printf("%d\n", compute(add, 5, 6)); // 11
+	printf("%d\n", compute(subtract, 5, 6)); // -1
+
+	return 0;
+}
+
 int add(int a, int b) {
 	return a+b;
 }
@@ -14,15 +27,6 @@ int subtract(int a, int b) {
 	return a-b;
 }
 
-typedef int (*fptrOperation)(int, int);
-
-int compute(fptrOperation operation, int num1, int num2) {
+int compute(fp_operation operation, int num1, int num2) {
 	return operation(num1, num2);
-}
-
-int main() {
-	printf("%d\n", compute(add, 5, 6)); // 11
-	printf("%d\n", compute(subtract, 5, 6)); // -1
-
-	return 0;
 }
