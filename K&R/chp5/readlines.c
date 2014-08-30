217c592d379345641e158e8b7af4dfc9b119a6b8
wusuopubupt <wdxbupt2009@gmail.com>
2014-08-30 13:24:58 +0800

    update readlines.c (not working)

--- K&R/chp5/readlines.c
+++ K&R/chp5/readlines.c
@@ -2,28 +2,24 @@
 #include <string.h>
 #include <stdlib.h>
 
-#define MAXLINES  5
+#define MAXLINES  100
 #define MAXLEN    100
 
-char *lineptr[MAXLINES];
+static int get_line(char s[], int max_len);
+int readlines(char *lineptr[], int maxlines);
+void writelines(char *lineptr[], int maxlines);
 
-int getline(char s[], int max_len) {
+static int get_line(char s[], int max_len) {
 	int i = 0;
-//	int c;
-//	while(i < max_len && (c = getchar()) != EOF && c != '\n' ) {
-//		s[i++] = c;
-//	}
-//	if(c == '\n') {
-//		s[i++] = c;
-//	}
-//	s[i] = '\0';
+	int c;
 
-	*s = 'a';
-	*(s+1) = 'b';
-	*(s+2) = 'c';
-	*(s+3) = '\0';
-
-	i = 3;
+	while(i < max_len && (c = getchar()) != EOF && c != '\n') {
+		s[i++] = c;
+	}
+	if(c == '\n') {
+		s[i++] = c;
+	}
+	s[i] = '\0';
 
 	return i;
 }
@@ -32,12 +28,10 @@ int readlines(char *lineptr[], int maxlines) {
 	int i = 0;
 	int len;
 
-	char *line = lineptr[i++];
-
-
-	while(i < maxlines && (len = getline(line, MAXLEN)) > 0) {
-		printf("i: %d, line: %s\n", i, line);
-		line = lineptr[i++];
+	char *line = lineptr[i];
+	while(i < maxlines && (len = get_line(line, MAXLEN)) > 0) {
+		//printf("i: %d, line: %s", i, line);
+		line = lineptr[++i];
 	}
 	return i;
 }
@@ -45,19 +39,88 @@ int readlines(char *lineptr[], int maxlines) {
 void writelines(char *lineptr[], int n_line) {
 	int i = 0;
 	while(i < n_line) {
-		printf("%s\n", lineptr[i]);
+		printf("%s", lineptr[i]);
 		i++;
 	}
 }
-int main() {
-	int n_line;
+
+int main1() {
+	int flag, n_line;
+	int i = 0;
+	char **lineptr = (char **)malloc(sizeof(char *) * MAXLINES);
+	while(i < MAXLINES) {
+		lineptr[i] = (char *)malloc(sizeof(char) * MAXLEN);
+		i++;
+	}
 
 	if((n_line = readlines(lineptr, MAXLINES)) >= 0) {
+		printf("\n");
 		writelines(lineptr, n_line);
-		return 0;
+		flag = 1;
 	}
 	else {
 		printf("error\n");
+		flag = 0;
+	}
+
+	/* free */
+	if(lineptr) {
+		int i = 0;
+		while(i < MAXLINES) {
+			if(lineptr[i]) {
+				free(lineptr[i]);
+			}
+			i++;
+		}
+		free(lineptr);
+	}
+
+	if(flag) {
+		return 0;
+	}
+	else {
 		return 1;
 	}
 }
+
+int main() {
+	int c;
+	int i = 0;
+	char **lineptr = (char **)malloc(sizeof(char *) * MAXLINES);
+	while(i < MAXLINES) {
+		lineptr[i] = (char *)malloc(sizeof(char) * MAXLEN);
+		i++;
+	}
+
+	char *line = lineptr[0];
+	int j = 0;
+	while((c = getchar()) != EOF) {
+		if(c != '\n') {
+			line[j++] = c;
+		}
+		else {
+			line[j] = '\n';
+			line[j+1] = '\0';
+			j = 0;
+			line = lineptr[++i];
+		}
+	}
+
+	writelines(lineptr, i);
+
+	/* free */
+	if(lineptr) {
+		int i = 0;
+		while(i < MAXLINES) {
+			if(lineptr[i]) {
+				free(lineptr[i]);
+			}
+			i++;
+		}
+		free(lineptr);
+	}
+
+	return 0;
+}
+
+
