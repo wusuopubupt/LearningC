37c9d6649e5ed71e025437b99c2840394b06c808
wusuopubupt <wdxbupt2009@gmail.com>
2014-09-26 15:47:43 +0800

    create simple_mysql.c

---
+++ mysql/simple_mysql.c
@@ -0,0 +1,48 @@
+/*
+ * @author : wusuopubupt
+ * @desc   : simple MySQL C API
+ * @refer  : http://zetcode.com/db/mysqlc/
+ * @date   : 2014-09-26
+ *
+ *
+ * */
+#include <stdio.h>
+#include <stdlib.h>
+#include <mysql/mysql.h>
+
+int main() {
+	MYSQL *conn;
+	MYSQL_RES *res;
+	MYSQL_ROW row;
+
+	char *server = "localhost";
+	char *user = "root";
+	char *password = "yourpassword";
+	char *database = "mysql";
+
+	conn = mysql_init(NULL);
+
+	/* Connect to database */
+	if (!mysql_real_connect(conn, server, user, password, database, 0, NULL,
+			0)) {
+		fprintf(stderr, "%s\n", mysql_error(conn));
+		exit(1);
+	}
+
+	/* Send SQL query */
+	if (mysql_query(conn, "show tables")) {
+		fprintf(stderr, "%s\n", mysql_error(conn));
+		exit(1);
+	}
+
+	res = mysql_use_result(conn);
+	/* Output table name */
+	printf("MySQL Table in mysql database: \n");
+	while ((row = mysql_fetch_row(res)) != NULL) { // loop result
+		printf("%s\n", row[0]);
+	}
+
+	/* Close connection */
+	mysql_free_result(res);
+	mysql_close(conn);
+}
