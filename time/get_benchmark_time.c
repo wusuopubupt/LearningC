34f186afa40767c77d3ff7fd5900da797d8c2114
wusuopubupt <wdxbupt2009@gmail.com>
2014-09-26 16:44:50 +0800

    create get_benchmark_time.c

---
+++ time/get_benchmark_time.c
@@ -0,0 +1,31 @@
+/*
+ * @author  : wusuopubupt
+ * @desc	: function get_time() can be used in benchmark
+ * @date	: 2014-09-26
+ */
+#include <stdio.h>
+#include <sys/time.h>
+#include <sys/resource.h>
+
+double get_time() {
+	struct timeval t;
+	struct timezone tzp;
+	gettimeofday(&t, &tzp);
+	return t.tv_sec + t.tv_usec * 1e-6; // return second+millsecond
+}
+
+int main() {
+	double start_time = get_time();
+	increase_num();
+	double end_time = get_time();
+	printf("%f, start: %f, end:%f\n", end_time - start_time, start_time, end_time);
+
+	return 0;
+}
+
+void increase_num(void) {
+	long i;
+	for (i = 0; i < 10000000; i++) {
+		i++;
+	}
+}
