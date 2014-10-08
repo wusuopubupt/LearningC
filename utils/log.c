31a88652a9538140d44e4bd43602bbd485ffa45d
wusuopubupt <wdxbupt2009@gmail.com>
2014-10-08 16:46:05 +0800

    create log.c

---
+++ utils/log.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <time.h>
+#include <syslog.h>
+
+#define use_syslog 0
+#define TIME_FORMAT "%Y-%m-%d %H:%I:%S"
+
+#define LOG(format, ...) do {\
+    if (use_syslog) {\
+        syslog(LOG_ERR, format, ##__VA_ARGS__);\
+    } else {\
+        time_t now = time(NULL);\
+        char timestr[20];\
+        strftime(timestr, 20, TIME_FORMAT, localtime(&now));\
+        fprintf(stderr, "\e[01;35m %s ERROR: \e[0m" format "\n", timestr, ##__VA_ARGS__);\
+    }}\
+while(0)
+
+int main() {
+	LOG("undefined param time!");
+
+	return 0;
+}
